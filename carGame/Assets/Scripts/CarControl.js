6546010bb939bd2437573d27356fe1130d6cd515
Victor Porras <vporras@ymail.com>
2015-02-12 13:54:24 +0000

    adding unity game code

---
+++ carGame/Assets/Scripts/CarControl.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+var FL : WheelCollider;
+var FR : WheelCollider;
+var RL : WheelCollider;
+var RR : WheelCollider;
+var maxTorque : float;
+
+function Start () {
+	rigidbody.centerOfMass.y = -0.9;
+}
+
+function FixedUpdate () {
+	RR.motorTorque = maxTorque * Input.GetAxis("Vertical");
+	RL.motorTorque = maxTorque * Input.GetAxis("Vertical");
+	
+	FL.steerAngle = 10 * Input.GetAxis("Horizontal");
+	FR.steerAngle = 10 * Input.GetAxis("Horizontal");
+}
