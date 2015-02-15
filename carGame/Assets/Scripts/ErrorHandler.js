acd88f5617bd5ec522e3bc2b02a9d0b7f7d4dbf9
Victor Porras <vporras@ymail.com>
2015-02-15 01:56:15 +0000

    Adding error handling for bots. Error messages now print in wrapper.

---
+++ carGame/Assets/Scripts/ErrorHandler.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+function Awake () {
+	Application.RegisterLogCallback(HandleError);
+}
+
+function HandleError (logString : String, stackTrace : String, type : LogType) {
+	Application.ExternalCall("printError", logString, stackTrace, type);
+}
