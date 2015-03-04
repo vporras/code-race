092c6f8e1aa8238d71a029e98c9540298c7e2816
JohnBurden <john@johnburden.me>
2015-03-04 20:43:26 +0000

    Added very basic timing
    Need to still add detection for going through the starting line before
    timing. Also need to pass result to wrapper.
    Also improved starting line

---
+++ carGame/Assets/Scripts/Timer.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+import UnityEngine.UI;
+var elapsedTime: float;
+var guiTimer: Text;
+
+function Start () {
+  elapsedTime = 0.0;
+	
+
+}
+
+function Update () {
+elapsedTime+= Time.deltaTime;
+ guiTimer.text = "Time Elapsed " + elapsedTime.ToString() + "";
+}
