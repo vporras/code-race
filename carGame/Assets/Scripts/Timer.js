10de012096f711505b432ce52acece366a98975d
JohnBurden <john@johnburden.me>
2015-03-05 21:02:44 +0000

    Improved Timing
    Starts when you cross the line, ends when you cross the line.
    PrintScore function sends time to wrapper.

--- carGame/Assets/Scripts/Timer.js
+++ carGame/Assets/Scripts/Timer.js
@@ -1,15 +1,39 @@
 ﻿#pragma strict
 import UnityEngine.UI;
 var elapsedTime: float;
+var startTime: float;
 var guiTimer: Text;
+
+var lapno: int;
+
 
 function Start () {
   elapsedTime = 0.0;
+  startTime = 0.0;
+  lapno = 0;
 	
 
 }
 
 function Update () {
-elapsedTime+= Time.deltaTime;
- guiTimer.text = "Time Elapsed " + elapsedTime.ToString() + "";
+	if(lapno < 2) {
+		elapsedTime+= Time.deltaTime;
+ 		guiTimer.text = "Time Elapsed " + elapsedTime.ToString() + "";
+	}
+	else {
+	guiTimer.text = "Final Time: " + elapsedTime.ToString();
+	}
+}
+
+function OnTriggerEnter() {
+	
+	if(lapno == 0) {
+		elapsedTime = 0; lapno = lapno+1;
+	}
+	
+	else {
+	elapsedTime = elapsedTime; lapno = lapno+1;
+	Application.ExternalCall("printScore(" + elapsedTime +")");
+	}
+	
 }
