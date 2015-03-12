68d38ef22bfc7851d83bcfdf9fcf8451abdf1470
Eris Frison <ilya.crols@gmail.com>
2015-03-12 17:38:04 +0000

    Camera switching with numpad 0/1

--- carGame/Assets/Scripts/CarCamera.js
+++ carGame/Assets/Scripts/CarCamera.js
@@ -7,25 +7,42 @@ var heightDamping : float = 2.0;
 var zoomRatio : float = 0.5;
 var DefaultFOV : float = 60;
 private var rotationVector : Vector3;
+private var manual : boolean = false;
 
 function Start () {
 
 }
 
 function LateUpdate () {
-	var wantedAngle = rotationVector.y;
-	var wantedHeight = car.position.y + height;
-	var myAngle = transform.eulerAngles.y;
-	var myHeight = transform.position.y;
-	
-	myAngle = Mathf.LerpAngle(myAngle, wantedAngle, rotationDamping * Time.deltaTime);
-	myHeight = Mathf.Lerp(myHeight, wantedHeight, heightDamping * Time.deltaTime);
-	
-	var currentRotation = Quaternion.Euler(0, myAngle, 0);
-	transform.position = car.position;
-	transform.position -= currentRotation * Vector3.forward * distance;
-	transform.position.y = myHeight;
-	transform.LookAt(car);
+	if(!manual){
+		var wantedAngle = rotationVector.y;
+		var wantedHeight = car.position.y + height;
+		var myAngle = transform.eulerAngles.y;
+		var myHeight = transform.position.y;
+		
+		myAngle = Mathf.LerpAngle(myAngle, wantedAngle, rotationDamping * Time.deltaTime);
+		myHeight = Mathf.Lerp(myHeight, wantedHeight, heightDamping * Time.deltaTime);
+		
+		var currentRotation = Quaternion.Euler(0, myAngle, 0);
+		transform.position = car.position;
+		transform.position -= currentRotation * Vector3.forward * distance;
+		transform.position.y = myHeight;
+		transform.LookAt(car);
+	}
+	else {
+		var turnSpeed = 3;
+		transform.RotateAround(transform.position, transform.right, -Input.GetAxis("Mouse Y") * turnSpeed);
+		transform.RotateAround(transform.position, Vector3.up, Input.GetAxis("Mouse X") * turnSpeed);
+		
+		var speed : float = 30;
+		transform.Translate(Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0.0, Input.GetAxis("Vertical") * speed * Time.deltaTime));
+	}
+	if(Input.GetKeyDown(KeyCode.Keypad0)){
+		manual=true;
+	}
+	if(Input.GetKeyDown(KeyCode.Keypad1)){
+		manual=false;
+	}
 }
 
 
